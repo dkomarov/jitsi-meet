e07e68d583e9e1aada0840d2f1d9a80da7e8c57f
dkomarov <dkomarov@hawk.iit.edu>
2022-01-02 21:07:31 -0600

    return global styles (try to restore toolbar)

--- react/features/app/components/App.web.js
+++ react/features/app/components/App.web.js
@@ -4,6 +4,7 @@ import { AtlasKitThemeProvider } from '@atlaskit/theme';
 import React from 'react';
 
 import { DialogContainer } from '../../base/dialog';
+import GlobalStyles from '../../base/ui/components/GlobalStyles';
 import JitsiThemeProvider from '../../base/ui/components/JitsiThemeProvider';
 import { ChromeExtensionBanner } from '../../chrome-extension-banner';
 
@@ -28,6 +29,7 @@ export class App extends AbstractApp {
     _createMainElement(component, props) {
         return (
             <JitsiThemeProvider>
+            <GlobalStyles />
                 <AtlasKitThemeProvider mode = 'dark'>
                     <ChromeExtensionBanner />
                     { super._createMainElement(component, props) }
