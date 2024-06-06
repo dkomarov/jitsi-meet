1463388109853387076acf9cf237366f13f76b67
dkomarov <dkomarov@hawk.iit.edu>
2024-06-05 19:38:26 -0500

    expand react state logic to toolbar file

--- react/features/always-on-top/ToolbarButton.tsx
+++ react/features/always-on-top/ToolbarButton.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 
 import Icon from '../base/icons/components/Icon';
 
@@ -37,8 +37,7 @@ interface IProps {
     toggled?: boolean;
 }
 
-export const DEFAULT_COLOR =
-    navigator.product === 'ReactNative' ? 'white' : undefined;
+export const DEFAULT_COLOR = navigator.product === 'ReactNative' ? 'white' : undefined;
 export const DEFAULT_SIZE = navigator.product === 'ReactNative' ? 36 : 40; // 22
 
 const ToolbarButton = ({
@@ -49,10 +48,79 @@ const ToolbarButton = ({
     icon,
     toggled = false
 }: IProps) => {
-    const [iconColor, setColor] = useState(DEFAULT_COLOR);
+    const [iconColor, setColor] = useState('gold');
     const [hasColorChanged, setHasColorChanged] = useState(false);
-    const [iconSize, setSize] = useState(DEFAULT_SIZE);
-    const [sizeClassName, setHasSizeChanged] = useState('');
+    const [iconSize, setSize] = useState(36);
+    const [sizeClassName, setSizeClassName] = useState('size-medium');
+
+    const elements = document.querySelectorAll(
+        '.jitsi-icon-default svg:not(.settings-button-small-icon svg, #participant-connection-indicator svg, #mic-disabled, #toggleFilmstripButton svg)'
+    );
+
+    useEffect(() => {
+        const applyStyles = () => {
+            try {
+                elements.forEach((icon) => {
+                    if (iconColor != null && iconColor != '' && iconColor != undefined) {
+                        // @ts-ignore
+                        icon.parentElement.classList.add(iconColor);
+                    }
+
+                    if (sizeClassName != null && sizeClassName != '' && sizeClassName != undefined) {
+                        // @ts-ignore
+                        icon.parentElement.classList.add(sizeClassName);
+                    }
+                    // if (icon instanceof HTMLElement) {
+
+                    //     icon.style.fill = iconColor;
+                    //     icon.style.width = `${iconSize}px`;
+                    //     icon.style.height = `${iconSize}px`;
+                    // } else if (icon instanceof SVGElement) {
+                    //     icon.setAttribute('fill', iconColor);
+                    //     icon.style.width = `${iconSize}px`;
+                    //     icon.style.height = `${iconSize}px`;
+                    // }
+                });
+            } catch (err) {
+                console.log('Error re-applying styles:', err);
+            }
+        };
+
+        window.addEventListener('resize', applyStyles);
+
+        // Apply styles initially
+        applyStyles();
+
+        return () => {
+            // window.removeEventListener('resize', applyStyles);
+        };
+    }, [iconColor, iconSize]);
+
+    useEffect(() => {
+        localStorage.setItem('icon_color_class', JSON.stringify(iconColor));
+        console.log("localStorage.getItem('icon_color_class') is now:", localStorage.getItem('icon_color_class'));
+
+        localStorage.setItem('icon_size_class', JSON.stringify(sizeClassName));
+        console.log("localStorage.getItem('icon_size_class') is now:", localStorage.getItem('icon_size_class'));
+    }, [iconColor, sizeClassName]);
+
+    useEffect(() => {
+        try {
+            let iconData = window.localStorage.getItem('icon_color_class');
+
+            if (iconData != null && iconData != '' && iconData != undefined) setColor(iconData);
+        } catch (err) {
+            console.log('error getting/parsing iconData in local storage:', err);
+        }
+
+        try {
+            let sizeData = window.localStorage.getItem('icon_size_class');
+
+            if (sizeData != null && sizeData != '' && sizeData != undefined) setSizeClassName(sizeData);
+        } catch (err) {
+            console.log('error getting/parsing sizeData in local storage:', err);
+        }
+    }, [iconColor, sizeClassName]);
 
     const onKeyPress = useCallback(
         (event) => {
@@ -79,26 +147,20 @@ const ToolbarButton = ({
     };
 
     window.addEventListener('message', function (event) {
-        if (
-            typeof event.data === 'string' &&
-            event.data.includes('Selected jitsi-icon color: ')
-        ) {
+        if (typeof event.data === 'string' && event.data.includes('Selected jitsi-icon color: ')) {
             console.log('Message received from the parent: ' + event.data); // Message received from parent
             dataColor = event.data.split(': ')[1].toString().trim();
             setColor(dataColor);
             setHasColorChanged(true);
         }
 
-        if (
-            typeof event.data === 'string' &&
-            event.data.includes('Selected jitsi-icon size: ')
-        ) {
+        if (typeof event.data === 'string' && event.data.includes('Selected jitsi-icon size: ')) {
             console.log('Message received from the parent: ' + event.data); // Message received from parent
             dataSize = parseInt(event.data.split(': ')[1].toString().trim());
             for (let key in size_class) {
                 let x: number = parseInt(key);
                 if (x === dataSize) {
-                    setHasSizeChanged(size_class[x].toString());
+                    setSizeClassName(size_class[x].toString());
                 }
             }
             setSize(dataSize);
@@ -110,15 +172,15 @@ const ToolbarButton = ({
             aria-disabled={disabled}
             aria-label={accessibilityLabel}
             aria-pressed={toggled}
-            className={`toolbox-button ${disabled ? ' disabled' : ''}`}
+            className={`toolbox-button ${disabled ? ' disabled' : ''} ${iconColor || ''} ${sizeClassName || ''}`}
             onClick={disabled ? undefined : onClick}
             onKeyPress={disabled ? undefined : onKeyPress}
             role="button"
             tabIndex={0}
         >
             <div
-                className={`toolbox-icon ${disabled ? 'disabled' : ''} ${
-                    customClass ?? ''
+                className={`toolbox-icon ${disabled ? 'disabled' : ''} ${customClass ?? ''} ${iconColor || ''} ${
+                    sizeClassName || ''
                 }`}
             >
                 <Icon src={icon} />
