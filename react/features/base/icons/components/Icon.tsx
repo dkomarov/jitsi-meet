60d11cc20c23abb3301c1b6345493ff851e956fb
dkomarov <dkomarov@hawk.iit.edu>
2024-06-07 15:39:53 -0500

    restore json parse method

--- react/features/base/icons/components/Icon.tsx
+++ react/features/base/icons/components/Icon.tsx
@@ -202,7 +202,7 @@ export default function Icon(props: IProps) {
             let iconData;
             if (window.localStorage.getItem('icon_color_class'))
                 iconData = window.localStorage.getItem('icon_color_class');
-            if (iconData != null && iconData != '' && iconData != undefined) setColor(iconData);
+            if (iconData != null && iconData != '' && iconData != undefined) setColor(JSON.parse(iconData));
         } catch (err) {
             console.log('error getting/parsing iconData in local storage:', err);
         }
@@ -210,7 +210,7 @@ export default function Icon(props: IProps) {
         try {
             let sizeData;
             if (window.localStorage.getItem('icon_size_class'))
-                if (sizeData != null && sizeData != '' && sizeData != undefined) setSizeClassName(sizeData);
+                if (sizeData != null && sizeData != '' && sizeData != undefined) setSizeClassName(JSON.parse(sizeData));
         } catch (err) {
             console.log('error getting/parsing sizeData in local storage:', err);
         }
